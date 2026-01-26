b894498524214919269bd812d862e7c569970d7e
Wagner Ferreira <wvinicius.torres@gmail.com>
2026-01-26 16:15:06 -0400

    componente de rodapé com informações do desenvolvedor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components_utils/header/header";
+import { Footer } from "./components_utils/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
